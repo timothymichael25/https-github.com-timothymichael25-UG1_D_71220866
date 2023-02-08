2954957797747a7f7d594a0bf9906ea088a31073
timothymichael25 <117884601+timothymichael25@users.noreply.github.com>
2023-02-08 19:07:32 +0700

    Add files via upload

---
+++ 2_B_71220866.py
@@ -0,0 +1,8 @@
+print("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
+p = input("Masukan operator matematika yang ingin Anda hitung: ")
+while True:
+    if a == '+':
+        aa = int(input("Mau penjumlahan berapa: "))
+        bb = int(input("Print berapa banyak: "))
+        for i in range (bb):
+            print(f'(i+1) + (bb-1) = [(i+1)+(bb-1)]')
